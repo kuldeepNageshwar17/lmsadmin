9c7525ddecaae191eab78ebcba1d269a4bdf9753
kuldeep nageshwar <nageshwar.kuldeep17@outlook.com>
2020-11-20 13:36:20 +0530

    InstituteFees

---
+++ src/app/modules/Setting/components/feesChangeFormatterBranch.js
@@ -0,0 +1,54 @@
+// please be familiar with react-bootstrap-table-next column formaters
+// https://react-bootstrap-table.github.io/react-bootstrap-table2/storybook/index.html?selectedKind=Work%20on%20Columns&selectedStory=Column%20Formatter&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel
+/* eslint-disable no-script-url,jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react'
+import SVG from 'react-inlinesvg'
+
+import { toAbsoluteUrl } from '../../../../_metronic/_helpers'
+
+export default function FeesChangeActionFormatter ({
+  cellContent,
+  row,
+  rowIndex,
+  updateFees
+}) {
+  const [IsInput, setIsInput] = useState(false)
+  const [feesValue, setFeesValue] = useState(cellContent)
+  const Edit = () => {
+    setIsInput(true)
+  }
+  const save = () => {
+    updateFees(row._id, feesValue)
+    setIsInput(false)
+  }
+
+  return (
+    <>
+      {IsInput && (
+        <>
+           <button onClick={save} className={'btn btn-success'} style={{"display":"inline"}}>
+            save
+          </button>
+          <input
+            value={feesValue}
+            onChange={event => {
+              setFeesValue(event.target.value)
+            }}
+          />
+       
+        </>
+      )}
+      {!IsInput && (
+        <>
+          <div>
+          <button onClick={Edit} className={'btn btn-primary '} style={{"display":"inline"}}>
+              edit
+            </button>
+            {feesValue}
+            
+          </div>
+        </>
+      )}
+    </>
+  )
+}
--- src/app/modules/Setting/pages/classesForBranch.js
+++ src/app/modules/Setting/pages/classesForBranch.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import input from "util"
-import {  Button, Form } from 'react-bootstrap'
+import input from 'util'
+import { Button, Form } from 'react-bootstrap'
 import {
   Card,
   CardBody,
@@ -15,34 +15,41 @@ import BootstrapTable from 'react-bootstrap-table-next'
 import paginationFactory, {
   PaginationProvider
 } from 'react-bootstrap-table2-paginator'
-import ClassActionFormatter from '../components/classActionFormatterForBranchAdmin'
+import ClassActionFormatter from '../components/classActionFormatterBranch'
+import FeesChangeFormatter from '../components/feesChangeFormatterBranch'
 
-export default function ClassesForBranch(props) {
+export default function ClassesForBranch (props) {
   const [Classes, setClasses] = useState(null)
-  const [singleClass, setSingleClass] = useState()
+  //  const [singleClass, setSingleClass] = useState()
 
-  const EditClassHandler = (id, name, ClassFees) => {
-    setSingleClass({ ...singleClass, id: id, name: name, ClassFees })
-  }
+  // const EditClassHandler = (id, name, ClassFees) => {
+  // }
 
-  const DeleteClassHandler = id => {
-    if (window.confirm('do you really  want to delete')) {
-      axios
-        .delete('/api/branch/class/' + id)
-        .then(res => {
-          updateData()
-        })
-        .catch(() => { })
-    }
-  }
-  const changeClassFees = (value) => {
-    setSingleClass({ ...singleClass, ClassFees: value })
-  }
-  const getCoursesHandler = id => {
-    props.history.push('/setting/Courses/' + id)
-  }
-  const ChangeDescription = (cellContent) => {
+  // const DeleteClassHandler = id => {
+  //   if (window.confirm('do you really  want to delete')) {
+  //     axios
+  //       .delete('/api/branch/class/' + id)
+  //       .then(res => {
+  //         updateData()
+  //       })
+  //       .catch(() => {})
+  //   }
+  // }
+  
+  // const getCoursesHandler = id => {
+  //   props.history.push('/setting/Courses/' + id)
+  // }
+  const FormatDescription = cellContent => {
     return <div dangerouslySetInnerHTML={{ __html: cellContent }}></div>
+  }
+
+  const feesFormatter = (cellContent, row, rowIndex) => {
+    return (
+      <>
+        {/* {cellContent} */}
+        <FeesChangeFormatter cellContent={cellContent} row={row} rowIndex={rowIndex}  updateFees={updateFees}/>
+      </>
+    )
   }
 
   const options = {
@@ -74,7 +81,7 @@ export default function ClassesForBranch(props) {
       dataField: 'description',
       text: 'Description',
       sort: true,
-      formatter: ChangeDescription,
+      formatter: FormatDescription,
       sortCaret: sortCaret,
       headerSortingClasses
     },
@@ -82,38 +89,32 @@ export default function ClassesForBranch(props) {
       dataField: 'fees',
       text: 'fees',
       sort: true,
-
+      formatter: feesFormatter
+      // formatExtraData: {
+      // },
     },
-    {
-      dataField: 'ClassFees',
-      text: 'ClassFees',
-      sort: true,
-
-    },
-
-
-    {
-      dataField: 'action',
-      text: 'Actions',
-      formatter: ClassActionFormatter,
-      formatExtraData: {
-        EditClassAction: EditClassHandler,
-        DeleteClassAction: DeleteClassHandler,
-        getCoursesAction: getCoursesHandler
-      },
-      classes: 'text-right pr-0',
-      headerClasses: 'text-right pr-3',
-      style: {
-        minWidth: '250px'
-      }
-    }
+    // {
+    //   dataField: 'action',
+    //   text: 'Actions',
+    //   formatter: ClassActionFormatter,
+    //   formatExtraData: {
+    //     EditClassAction: EditClassHandler,
+    //     DeleteClassAction: DeleteClassHandler,
+    //     getCoursesAction: getCoursesHandler
+    //   },
+    //   classes: 'text-right pr-0',
+    //   headerClasses: 'text-right pr-3',
+    //   style: {
+    //     minWidth: '250px'
+    //   }
+    // }
   ]
   const updateData = () => {
     debugger
     axios
-      .get('/api/Branch/classes')
+      .get('/api/Branch/getBranchClasses')
       .then(res => {
-        console.log("data here", res.data)
+        console.log('data here', res.data)
         setClasses(res.data)
       })
       .catch(err => {
@@ -121,65 +122,62 @@ export default function ClassesForBranch(props) {
       })
   }
 
-
   useEffect(() => {
     updateData()
   }, [])
 
-  const submitFee = () => {
-    axios.post("/api/Branch/setClassFees", singleClass).then((res) => {
-
-    }).catch((error) => {
-
-    })
+  const updateFees = (classId,fees) => {
+    axios
+      .post('/api/Branch/setClassFees/',{classId,fees})
+      .then(res => {})
+      .catch(error => {})
     updateData()
-
   }
   return (
     <div>
-              <div className='row'>
-                <div className='col-md-12'>
-                  <Card>
-                    <CardHeader title='All Class'>
-                      <CardHeaderToolbar>
-                        <button
-                          type='button'
-                          className='btn btn-primary'
-                          onClick={() => {
-                            props.history.push('/setting/classForm')
-                          }}
-                        >
-                          New Class
+      <div className='row'>
+        <div className='col-md-12'>
+          <Card>
+            <CardHeader title='All Class'>
+              {/* <CardHeaderToolbar>
+                <button
+                  type='button'
+                  className='btn btn-primary'
+                  onClick={() => {
+                    props.history.push('/setting/classForm')
+                  }}
+                >
+                  New Class
                 </button>
-                      </CardHeaderToolbar>
-                    </CardHeader>
-                    <CardBody>
-                      {Classes ? (
-                        <PaginationProvider pagination={paginationFactory(options)}>
-                          {({ paginationProps, paginationTableProps }) => {
-                            return (
-                              <BootstrapTable
-                                keyField='_id'
-                                data={Classes}
-                                columns={columns}
-                                classes='table table-head-custom table-vertical-center overflow-hidden'
-                                wrapperClasses='table-responsive'
-                                bootstrap4
-                                remote
-                                bordered={false}
-                                pagination={paginationFactory(options)}
-                                {...paginationTableProps}
-                              />
-                            )
-                          }}
-                        </PaginationProvider>
-                      ) : (
-                          <div>loading</div>
-                        )}
-                    </CardBody>
-                  </Card>
-                </div>
-              </div>
-            </div>
+              </CardHeaderToolbar> */}
+            </CardHeader>
+            <CardBody>
+              {Classes ? (
+                <PaginationProvider pagination={paginationFactory(options)}>
+                  {({ paginationProps, paginationTableProps }) => {
+                    return (
+                      <BootstrapTable
+                        keyField='_id'
+                        data={Classes}
+                        columns={columns}
+                        classes='table table-head-custom table-vertical-center overflow-hidden'
+                        wrapperClasses='table-responsive'
+                        bootstrap4
+                        remote
+                        bordered={false}
+                        pagination={paginationFactory(options)}
+                        {...paginationTableProps}
+                      />
+                    )
+                  }}
+                </PaginationProvider>
+              ) : (
+                <div>loading</div>
+              )}
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    </div>
   )
 }
